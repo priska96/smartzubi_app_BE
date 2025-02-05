3fc2267b7625262c0d2869ab21543049aa6d4dcc
priska <priskakohnen@gmail.com>
2025-02-05 20:16:03 +0100

    change envConfs for deployment

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from .envConfig import Config
 
-SQLALCHEMY_DATABASE_URL = "postgresql://"+ Config.DB_HOSTNAME+":"+Config.DB_PASSWORD+"@localhost/postgres"
+SQLALCHEMY_DATABASE_URL = Config.DATABASE_URL
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- app/envConfig.py
+++ app/envConfig.py
@@ -6,8 +6,9 @@ dotenv_file = f".env.{environment}"
 load_dotenv(dotenv_file)
 
 class Config:
-    YOUR_DOMAIN = os.getenv("DOMAIN_URL")
-    STRIPE_KEY = os.getenv("STRIPE_API_KEY_DEV")
-    ENVIRONMENT = environment
-    DB_HOSTNAME = os.getenv("DB_HOSTNAME") 
-    DB_PASSWORD = os.getenv("DB_PASSWORD")
+    YOUR_DOMAIN = os.environ.get('DOMAIN_URL') #os.getenv("DOMAIN_URL")
+    STRIPE_KEY = os.environ.get('STRIPE_API_KEY_DEV') #os.getenv("STRIPE_API_KEY_DEV")
+    ENVIRONMENT = "dev"#environment
+    DB_HOSTNAME = os.environ.get('DB_HOSTNAME') #os.getenv("DB_HOSTNAME") 
+    DB_PASSWORD = os.environ.get('DB_PASSWORD') #os.getenv("DB_PASSWORD")
+    DATABASE_URL = os.environ.get('DATABASE_URL',"postgresql://"+ DB_HOSTNAME+":"+DB_PASSWORD+"@localhost/postgres")
