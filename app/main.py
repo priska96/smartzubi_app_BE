6ae4c87dac3a21a0d237e5df86e2f937234028ec
priska <priskakohnen@gmail.com>
2025-02-05 21:01:11 +0100

    add debug print and remove deveice api endpint

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+#from fastapi.staticfiles import StaticFiles
+#from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -44,7 +44,8 @@ app.add_middleware(
 )
 
 
-
+print ("environment", Config.ENVIRONMENT, Config.YOUR_DOMAIN, Config.STRIPE_KEY, Config.DATABASE_URL)
+    
 Base.metadata.create_all(bind=engine)
 
 
@@ -55,30 +56,30 @@ app.include_router(exam.router)
 app.include_router(payment.router)
 
 
-@app.post("/api/devices/", response_model=DeviceResponse)
-@token_required
-async def create_device(
-    obj_in: DeviceCreate,
-    db: Session = Depends(get_db),
-    dependencies=Depends(jwt_bearer),
-):
-    Device.create_device(obj_in, db)
+# @app.post("/api/devices/", response_model=DeviceResponse)
+# @token_required
+# async def create_device(
+#     obj_in: DeviceCreate,
+#     db: Session = Depends(get_db),
+#     dependencies=Depends(jwt_bearer),
+# ):
+#     Device.create_device(obj_in, db)
 
 
-@app.get("/api/users/{user_id}/devices/", response_model=list[DeviceResponse])
-@token_required
-async def get_device(
-    user_id: int, db: Session = Depends(get_db), dependencies=Depends(jwt_bearer)
-):
-    Device.get_device(user_id, db)
+# @app.get("/api/users/{user_id}/devices/", response_model=list[DeviceResponse])
+# @token_required
+# async def get_device(
+#     user_id: int, db: Session = Depends(get_db), dependencies=Depends(jwt_bearer)
+# ):
+#     Device.get_device(user_id, db)
 
 
-@app.delete("/api/devices/{device_id}", response_model=DeviceResponse)
-@token_required
-async def delete_device(
-    device_id: int, db: Session = Depends(get_db), dependencies=Depends(jwt_bearer)
-):
-    Device.delete_device(device_id, db)
+# @app.delete("/api/devices/{device_id}", response_model=DeviceResponse)
+# @token_required
+# async def delete_device(
+#     device_id: int, db: Session = Depends(get_db), dependencies=Depends(jwt_bearer)
+# ):
+#     Device.delete_device(device_id, db)
 
 
 @app.get("/api/check-env")
