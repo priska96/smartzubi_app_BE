46a8a7a11690c4f4ecdcc58d9590bb605c67a769
priska <priskakohnen@gmail.com>
2025-02-07 19:26:21 +0100

    run last fixture

--- app/fixtures/load_fixtures.py
+++ app/fixtures/load_fixtures.py
@@ -13,16 +13,17 @@ def load_jsons():
         # current_directory+"/app/fixtures/exam_2024_3.json"
         # current_directory
         # + "/app/fixtures/notion_temp.json"
-        current_directory
-        + "/app/fixtures/menu_wine_exam.json",
-        current_directory
-        + "/app/fixtures/warenannahme_lagerkennzahlen_exam.json",
+        # current_directory
+        # + "/app/fixtures/menu_wine_exam.json",
+        # current_directory
+        # + "/app/fixtures/warenannahme_lagerkennzahlen_exam.json",
         current_directory
         + "/app/fixtures/bedarfsermittlung_exam.json"
     ]
 
     for file_name in files:
         with open(file_name, "r") as json_file:
+            print("Open file:", file_name)
             data = json.load(json_file)
             Exam.create(ExamCreateReq(**data), SessionLocal())
 
